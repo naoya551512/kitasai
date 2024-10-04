63be64550b75853aefa01c0fd769974b55be5972
matsumotonaoya <naoya1575.mei@gmail.com>
2024-10-04 19:13:25 +0900

    first commit

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import os
-from flask import Flask, abort, request
-from linebot.v3.webhook import (
-    WebhookHandler
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
 )
-from linebot.v3.exceptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
 )
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
-
 
 app = Flask(__name__)
 
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -38,34 +37,17 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        #相手の送信した内容で条件分岐して回答を変数に代入
-        if event.message.text == 'グー':
-            msg = 'パー'
-        elif event.message.text == 'チョキ':
-            msg = 'グー'
-        elif event.message.text == 'パー':
-            msg = 'チョキ'
-        else:
-            msg = 'ごめんね。\nまだ他のメッセージには対応してないよ'
-
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=msg)]
-            )
-        )
-
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
